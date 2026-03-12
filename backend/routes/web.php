80d7ff952ac3af4f4a3911e62c1da23e08bc411d
Mohamed Rahoumi <mohamed@example.com>
2026-03-12 01:25:43 +0000

    GÃ©nÃ©rer la migration presences

---
+++ backend/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+// All logic has been migrated to API (routes/api.php)
